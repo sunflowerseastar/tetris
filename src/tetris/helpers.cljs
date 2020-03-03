38abb4e821365afc6e9d27af98d4efe1a34077da
Grant Surlyn <grant@sunflowerseastar.com>
2020-03-02 23:18:01 -0800

    abstract some-squares

--- src/tetris/helpers.cljs
+++ src/tetris/helpers.cljs
@@ -6,16 +6,18 @@
 (defn random-up-to [n]
   (js/parseInt (* (.random js/Math) n)))
 
+(defn some-squares-are-non-empty-p [squares board]
+  (->> squares
+       (map (fn [square] (get-square (:x square) (inc (:y square)) board)))
+       (filter #(not (= (:active %) true)))
+       (map empty?)
+       (some false?)))
+
 (defn piece-can-move-down-p [board board-height]
   (let [actives (->> board
                      flatten
                      (filter #(= (:active %) true)))
         max-y (->> actives (map :y) (reduce max))
         has-reached-bottom-p (>= (inc max-y) board-height)]
-    (if has-reached-bottom-p false
-        (let [some-squares-are-non-empty-p (->> actives
-                                                (map (fn [square] (get-square (:x square) (inc (:y square)) board)))
-                                                (filter #(not (:active %)))
-                                                (map empty?)
-                                                (some false?))]
-          (not some-squares-are-non-empty-p)))))
+    (println actives)
+    (and (not has-reached-bottom-p) (not (some-squares-are-non-empty-p actives board)))))
