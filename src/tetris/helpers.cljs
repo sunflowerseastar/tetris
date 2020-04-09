003dca6cbc1c2946d73d3d951e429badea889f23
Grant Surlyn <grant@sunflowerseastar.com>
2020-04-09 00:18:01 -0700

    in-bounds corrections

--- src/tetris/helpers.cljs
+++ src/tetris/helpers.cljs
@@ -17,9 +17,15 @@
 (defn not-active-p [square]
   (not (= (:active square) true)))
 
+(defn x-y-in-bounds? [x-y board]
+  (let [x (first x-y) y (second x-y)
+        x-in-bounds (and (>= x 0) (< x (count (first board))))
+        y-in-bounds (and (>= y 0) (< y (count board)))]
+    (and x-in-bounds y-in-bounds)))
+
 (defn get-x-y [board x-y]
   (let [x (first x-y) y (second x-y)]
-    (if (and (pos? x) (<= x (count (first board))))
+    (if (x-y-in-bounds? x-y board)
       (get-square x y board)
       nil)))
 
@@ -27,11 +33,13 @@
   (let [board-width (count (first board))
         board-height (count board)
         xs (map first xs-ys)
+        ys (map second xs-ys)
         max-x (reduce max xs)
         min-x (reduce min xs)
-        has-exceeded-right-edge (>= max-x board-width)
-        has-exceeded-left-edge (< min-x 0)]
-    (and (not has-exceeded-left-edge) (not has-exceeded-right-edge))))
+        x-in-bounds (and (>= min-x 0) (< max-x board-width))
+        max-y (reduce max ys)
+        y-in-bounds (< max-y board-height)]
+    (and x-in-bounds y-in-bounds)))
 
 (defn xs-ys-are-free? [xs-ys board]
   (->> xs-ys
@@ -92,8 +100,8 @@
 
 (defn piece-can-rotate-p [piece-type board]
   (let [new-xs-ys (board->rotated-active-xs-ys piece-type board)
-        xs-ys-in-bounds (xs-ys-in-bounds? new-xs-ys board)]
-    (and xs-ys-in-bounds (xs-ys-are-free? new-xs-ys board))))
+        in-bounds (xs-ys-in-bounds? new-xs-ys board)]
+    (and in-bounds (xs-ys-are-free? new-xs-ys board))))
 
 (defn board->shifted-right-active-xs-ys [board]
   (map (fn [{:keys [x y]}] [(inc x) y]) (get-actives board)))
